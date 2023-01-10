9a702e509758d75e19d5909ace7aefd9cf103df7
chuazz <anhsonbeo97@gmail.com>
2023-01-10 11:43:54 +0700

    Just update :)))

--- src/assets/components/header/components/Navigation/NavigationActive/main.js
+++ src/assets/components/header/components/Navigation/NavigationActive/main.js
@@ -1,29 +1,53 @@
 // Framework
 import clsx from "clsx";
+import { memo, useEffect, useState } from "react";
+
+// Component
+import Accommodation from "./Accommodation/main.js";
 
 // Style
 import styles from "./NavigationActive.module.scss";
 
 function NavigationActive() {
+    const [options, setOptions] = useState([]);
+    const [activeOption, setActiveOption] = useState(1);
+    const [activeOptionBlock, setActiveOptionBlock] = useState(1);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/options")
+            .then(response => response.json())
+            .then(data => setOptions(data))
+    }, []);
+
+    const itemHandleClick = (id) => {
+        setActiveOption(id)
+    }
+
     return (
-        <div
-            className={clsx(styles.container)}
-        >
+        <div className={clsx(styles.container)}>
             <div className="row ali-center jus-center">
-                <div className={clsx(styles.item, "p-l-24")}>
-                    <p>Chỗ ở</p>
-                </div>
+                {options.map(option => (
+                    <div 
+                        key={option.id} 
+                        className={clsx(
+                            styles.item,
+                            {
+                                [styles.active]: option.id === activeOption
+                            }
+                        )}
+                        onClick={() => itemHandleClick(option.id)}
+                    >
+                        <p>{option.name}</p>
+                        <div className={clsx(styles.customeHr)}></div>
+                    </div>
+                ))}
+            </div>
 
-                <div className={clsx(styles.item)}>
-                    <p>Trải nghiệm</p>
-                </div>
-
-                <div className={clsx(styles.item)}>
-                    <p>Trải nghiệm trực tuyến</p>
-                </div>
+            <div className={clsx(styles.activeBlock)}>
+                <Accommodation />
             </div>
         </div>
     );
 }
 
-export default NavigationActive;
+export default memo(NavigationActive);
